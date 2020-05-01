8f681b8ffeac22a1de4a56a17121f56d8ec81cd4
s.f.schouten <sfschouten@gmail.com>
2020-05-01 13:32:00 +0200

    started work on EpisodeLoader

--- meta-learning.py
+++ meta-learning.py
@@ -23,9 +23,42 @@ class LeopardEncoder(nn.Module):
         return x
 
 
-class EpisodeLoader()
-# TODO make dataloader that combines the iterators for each task
-# and bundles batches for those tasks into batches of episodes for meta-learning
+class EpisodeLoader(data.IterableDataset)
+"""
+Used to obtain episodes for meta-learning, i.e. batches of 'tasks' (support and query sets).
+Essentially a dataset of dataloaders.
+"""
+
+    def __init__(self, k, train_sets, valid_sets):
+        """
+        Params:
+          k: the amount of samples included in every support set.
+          datasets: the 
+        """
+        super(EpisodeLoader).__init__()
+
+        self.k = k
+        self.train_sets = train_sets
+        self.valid_sets = valid_sets
+
+
+    def __iter__(self, idx):
+        
+        worker_info = torch.utils.data.get_worker_info()
+        if worker_info is None:
+            nr_workers = 1
+            worker_id = 0
+        else:
+            nr_workers = worker_info.num_workers
+            worker_id = worker_info.id
+
+        for dataset in self.datasets:
+            setsize = len(dataset)
+            per_worker = int(math.ceil(setsize/float(nr_workers)))
+
+            iter_start = worker_id * per_worker
+            iter_end = min(iter_start + per_worker, setsize-1)
+
 
 
 def leopard():
@@ -44,13 +77,15 @@ def leopard():
     optimizer = optim.Adam(params, lr=beta)
     criterion = torch.nn.CrossEntropyLoss()    
 
+    episode_loader = EpisodeLoader()
+
     # outer loop
     while not done:
 
         optimizer.zero_grad()
 
         # sample batch of tasks
-        tasks = MagicTaskSampler.sample()
+        tasks = next(iter(episode_loader))
         total_loss = 0
 
         # inner loop
@@ -97,27 +132,29 @@ def leopard():
             # deep clones? then use that state dict for new 
             # f_theta_prime instance of model?
             # Use Tensor.clone so we can backprop through it and 
-            # update f_theta as well.
+            # update f_theta as well (if no first order approx).
 
 
             # TODO make sure that task-specific parameters in f_theta_prime
-            # as well as parameters in h_phi_prime, W and b and no others 
+            # as well as parameters in h_phi_prime, W and b 
             # have requires_grad = True.
+       
+            params = [
+                f_theta_prime.parameters(), 
+                h_phi_prime.parameters(),
+                [W, b]
+            ]
 
-            params = itertools.chain(
-                    f_theta_prime.parameters(), 
-                    h_phi_prime.parameters(),
-                    [W], [b]
-                )
+            params = itertools.chain(*params)
             task_optimizer = optim.Adam(params, lr=alpha)
             task_criterion = torch.nn.CrossEntropyLoss()    
             
-            def process_batch(batch_input):
-                batch_output = f_theta_prime(batch_input)       # d x k
+            def process_batch(batch):
+                batch_output = f_theta_prime(batch.input)       # d x k
                 batch_output = h_phi_prime(batch_output)        # l x k
                 batch_output = W @ batch_output + b             # N x k
                 batch_output = F.softmax(batch_output, dim=0)   # N x k
-                loss = task_criterion(batch_output, batch_target)
+                loss = task_criterion(batch_output, batch.target)
                 return loss
 
             # update task-specific parameters on support set (D_tr)
@@ -125,10 +162,7 @@ def leopard():
                 if step >= G - 1:
                     break
 
-                batch_input = batch.input                       # d x t x k
-                batch_target = batch.target
-               
-                loss = process_batch(batch_input)
+                loss = process_batch(batch)
                 
                 task_optimizer.zero_grad() 
                 loss.backward()
@@ -140,27 +174,37 @@ def leopard():
             # [3] Evaluate adapted params on query set, calc grads.
             
             if first_order_approx:
-                # TODO set all parameters in f_theta, h_phi, g_psi
-                # to have requires_grad = True
-            else:
                 # TODO set parameters in f_theta_prime, h_phi_prime, 
                 # g_psi to have requires_grad = True
-            
+            else:
+                # TODO set parameters in f_theta, h_phi, g_psi
+                # to have requires_grad = True
+                           
+
             # evaluate on query set (D_val) 
             for step, batch in enumerate(query_iter):
                 if step >= ??: #TODO name number, or is it always 1?
                     break
 
-                batch_input = batch.input                       # d x t x k
-                batch_target = batch.target
-
-                loss = process_batch(batch_input)
+                loss = process_batch(batch)
                 loss.backward()
 
+
             if first_order_approx:
-                # TODO copy gradients from primes to originals
+                with torch.no_grad():
+                    pairs = [(f_theta_prime, f_theta), (h_phi_prime, h_phi)]
+                    for prime, original in pairs:
+                        params = zip(prime.named_parameters(), original.named_parameters())
+                        for (pNamePr, pValueOr), (pNamePr, pValueOr) in params:
+                            if pNamePr != pNameOr:
+                                print(pNamePr)
+                                print(pNameOr)
+                                raise Error("Order in which named parameters are returned is probably not deterministic?")
+                            pValueOr.grad += pValuePr.grad.clone()
 
             # end of inner loop
+
+        #TODO divide gradients by number of tasks?
 
         optimizer.step()        
 
