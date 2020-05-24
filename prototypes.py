49fce1a0a2a4d5caf9164be6581b53141ac282a8
Sasafrass <albertharkema@gmail.com>
2020-05-24 20:07:46 +0200

    this should work before grid search

--- prototypes.py
+++ prototypes.py
@@ -74,7 +74,7 @@ def get_test_acc(config, val_bm, model, prototypes):
 
     return count / num
 
-def k_shot_test(config, val_loader, val_bms, model, optimizer):
+def k_shot_test(config, val_loader, val_bms, model, optimizer, test = False):
     """
     Function that retrains with k samples 
 
@@ -84,6 +84,7 @@ def k_shot_test(config, val_loader, val_bms, model, optimizer):
       val_bms: Batchmanager for validation task 
       Model, optimizer, criterion are standard NN parameters
       num_times: How many times we perform the k sample update
+      test: Toggles whether we're evaluating for test set or for validation set
     """
 
     # Iterate for how_many_updates we update
@@ -134,6 +135,9 @@ def k_shot_test(config, val_loader, val_bms, model, optimizer):
     # Print the list with validation accuracy
     test_acc = torch.tensor(test_acc)
     mean, std = torch.mean(test_acc), torch.std(test_acc)
+
+    if test:
+        return test_acc
     print(f'mean: {mean:.2f}, std: {std:.2f}', flush = True)
 
     # Write stuff to csv 
@@ -156,11 +160,11 @@ def write_to_csv(test_acc):
     test_list.append(torch.std(test_acc).item())
 
     # Actual writing of ints to csv
-    with open(csv_file_name, "w", newline="") as file:
+    with open(csv_file_name, "a", newline="") as file:
         writer = csv.writer(file)
         writer.writerow(test_list) 
 
-def run_prototype(config, train_bms, model, val_bms, sw):
+def run_prototype(config, train_bms, model, val_bms, sw, test_bm):
     """
     Function to run a prototypical network
 
@@ -193,6 +197,15 @@ def run_prototype(config, train_bms, model, val_bms, sw):
         num_workers=NUM_WORKERS
     )
 
+    # Make a test loader for test task
+    test_loader = EpisodeLoader.create_dataloader(
+        config.samples_per_support,
+        test_bm,
+        config.batch_size,
+        shuffle_labels = False, # in validation, we dont wanna
+        num_workers=NUM_WORKERS
+    )
+
     # Parameters
     params = model.parameters() 
 
@@ -219,6 +232,19 @@ def run_prototype(config, train_bms, model, val_bms, sw):
         # model.eval()
         # with torch.no_grad():
         #     run_epoch(config, val_loader, model, optimizer, criterion, val_loader = val_loader, sw = sw)
+
+    # Report final performance on test set 
+    test_acc = k_shot_test(config, test_loader, test_bm, model, optimizer, test = True)
+    mean, std = torch.mean(test_acc), torch.std(test_acc)
+    test_list = []
+    for tensor in test_acc:
+        test_list.append(tensor.item())
+    print("Final performance on test set: ")
+    print(f"Mean: {mean.item():.2f}, std: {std.item():.2f}")
+    print(test_list)
+
+    # Write test row as last row to csv
+    write_to_csv(test_acc)
 
 def run_epoch(config, episode_loader, model, optimizer, criterion, val_loader, sw):
     """
@@ -340,7 +366,7 @@ if __name__ == "__main__":
     parser.add_argument('--resume', action='store_true', help='resume training instead of restarting')
     parser.add_argument('--lr', type=float, help='learning rate', default = 2e-5)
     parser.add_argument('--nr_episodes', type=int, help='Number of episodes in an epoch', default = 25)
-    parser.add_argument('--epochs', type=int, help='Number of epochs', default = 25)
+    parser.add_argument('--epochs', type=int, help='Number of epochs', default = 1)
     parser.add_argument('--samples_per_support', type=int, help='Number of samples per each episode', default = 32)
     parser.add_argument('--nr_val_experiments', type=int, help='How many times we perform validation on SICK', default = 10)
 
@@ -351,6 +377,10 @@ if __name__ == "__main__":
     #parser.add_argument('--random_seed', type=int, default="42", help="Random seed")
     parser.add_argument('--val_task',  type=str, default="IBM" , help="Value for the validation task")
     parser.add_argument('--test_task', type=str, default="SICK", help="Value for the test task")
+    
+    # Model hyperparams
+    parser.add_argument('--num_layers', type=int, default=3, help="Number of BERT layers to fine-tune")
+
     ############################################
 
     # Retrieve argparse object and report some details
@@ -359,8 +389,15 @@ if __name__ == "__main__":
     print("Device: ", config.device)
     print("Number of episodes: ", config.nr_episodes)
 
+    # Set the number of layers going down from 11 to 11-num_layers
+    trainable_layers = []
+    for i in reversed(range(11+1 - config.num_layers,11+1)):
+        trainable_layers.append(i)
+    assert len(trainable_layers) == config.num_layers
+
+    print("trainable_layers: ", trainable_layers)
+
     # Instantiate model
-    trainable_layers = [9,10,11]
     model = load_model(config, trainable_layers = trainable_layers)
 
     # All batchmanagers
@@ -382,8 +419,8 @@ if __name__ == "__main__":
     #train_bms.extend(batchmanager3.get_subtasks(2))
 
     """ Batchmanagers for validation and test task """
-    val_bms  = [batchmanager2] # IBM
-    test_bms = [batchmanager5] # SICK 
+    val_bms = [batchmanager5] # SICK
+    test_bm = [batchmanager2] # IBM 
 
     # To write results to sw and csv
     sw = SummaryWriter()
@@ -391,18 +428,20 @@ if __name__ == "__main__":
     # Be wary that csv_file_name is a global but it's in the same "block" as this
     if not os.path.exists("csv"):
         os.makedirs("csv")
-    csv_file_name = os.path.join("csv", "lr"+str(config.lr) + 
-                                 "_val" + config.val_task +
-                                 "_test" + config.test_task +
-                                 "_layers" + str(len(trainable_layers)) +
-                                 ".csv")
+
+    # Get model- and csv file name
+    model_name = os.path.join("csv", "lr"+str(config.lr) + 
+                              "_val" + config.val_task +
+                              "_test" + config.test_task +
+                              "_layers" + str(len(trainable_layers)))
+    csv_file_name = model_name + ".csv"       
 
     # Create column headers in csv file
     with open(csv_file_name, "w", newline= "") as file:
 
         # numbers + "std" as header
         write_list = []
-        for i in range(1, config.nr_val_experiments):
+        for i in range(1, config.nr_val_experiments + 1):
             write_list.append(i)
         write_list.append("std")
 
@@ -413,10 +452,9 @@ if __name__ == "__main__":
     # Train the model
     print('Beginning the training...', flush = True)
     #state_dict, dev_acc = protomaml(config, batchmanagers, BERT)
-    run_prototype(config, train_bms, model, val_bms, sw)
+    run_prototype(config, train_bms, model, val_bms, sw, test_bm)
     print("Finished the run_prototype function!")
-    
-    #print(f"#*#*#*#*#*#*#*#*#*#*#\nFINAL BEST DEV ACC: {dev_acc}\n#*#*#*#*#*#*#*#*#*#*#", flush = True)
 
     #save model
-    #torch.save(state_dict, path_to_dicts(config))
+    model_path = os.path.join("state_dicts", model_name + ".pth")
+    torch.save(model.state_dict(), model_path)
